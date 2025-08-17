0296d04c42b5b9b41e0c3b15e992b9ad3bfbce02
Dmytro <dmutro15um10@gmail.com>
2025-08-17 08:51:42 +0200

    hw 6

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,28 @@
-function getElementWidth(content, padding, border) {
-  let numContent = parseFloat(content);
-  let numPadding = parseFloat(padding);
-  let numBorder = parseFloat(border);
-  let fullWidth = numContent + (numPadding + numBorder) * 2;
-  return fullWidth;
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
 }
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
